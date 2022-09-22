e8c2a31a0736b3b56020a666da101f266e3ca12e
n0deee <diogodogogo@gmail.com>
2022-09-21 23:52:15 -0300

    ChannelManager Created

--- src/classes.ts
+++ src/classes.ts
@@ -1,66 +1,118 @@
-import { Socket } from "socket.io";
+import { Server, Socket } from "socket.io";
 import { Packets, PKicked, PUpdateNickname } from "./packets";
+
+export type ChannelIdentifier = number;
+export type MessageIdentifier = number;
+export type UserIdentifier = number;
+
+export enum ChannelType {
+    PUBLIC,
+    PRIVATE
+}
+
+export class UniquerId {
+    private _generated: number;
+
+    constructor(seed?: number) {
+        this._generated = seed || 0;
+    }
+
+    public getNext() {
+        this._generated += 1;
+        return this._generated;
+    }
+}
 
 export class User {
     public profile: UserProfile
-    private _userId: number
+    private _id: UserIdentifier;
 
-    constructor(userId: number, profile: UserProfile) {
-        this._userId = userId
+    constructor(id: UserIdentifier, profile: UserProfile) {
+        this._id = id;
         this.profile = profile
     }
 
-    get userId(): number {
-        return this._userId
-    }
+    get id() { return this._id }
 }
 
 export class UserProfile {
     public nickname: String
-    private _associatedUserId: number
+    private _associatedUserId: UserIdentifier;
 
-    constructor(associatedUserId: number, nickname: String) {
-        this._associatedUserId = associatedUserId
+    constructor(associatedUserId: UserIdentifier, nickname: String) {
+        this._associatedUserId = associatedUserId;
         this.nickname = nickname
     }
 
-    get userId(): number {
-        return this._associatedUserId
-    }
+    get userId() { return this._associatedUserId }
 }
 
 export class Message {
     public content: String
     public author: User
+    private _id: MessageIdentifier
 
-    constructor(content: String, author: User) {
+    constructor(id: MessageIdentifier, content: String, author: User) {
+        this._id = id;
         this.content = content
         this.author = author;
     }
+
+    get id() { return this._id }
 }
 
 export class Channel {
+    public channelType: ChannelType
+    public creator: UserIdentifier
     private _messages: Array<Message>
-    private _channelId: number
+    private _id: ChannelIdentifier
 
-    constructor(channelId: number) {
-        this._channelId = channelId
+    constructor(id: ChannelIdentifier, channelType: ChannelType, creator: UserIdentifier) {
+        this._id = id;
+        this.channelType = channelType;
+        this.creator = creator;
+
         this._messages = []
     }
 
-    get channelId() {
-        return this._channelId
+    public get id() { return this._id }
+}
+
+export class ChannelManager {
+    private _idGenerator: UniquerId
+    private _channels: Map<ChannelIdentifier, Channel>
+
+    constructor() {
+        this._idGenerator = new UniquerId();
+        this._channels = new Map<ChannelIdentifier, Channel>
     }
 
-    insertMessage(message: Message) {
-        this._messages.push(message)
+    public createChannel(channelType: ChannelType, creator: UserIdentifier): Channel {
+        const channelId = this._idGenerator.getNext()
+        const newChannel = new Channel(channelId, channelType, creator)
+
+        this._insertChannel(newChannel);
+        return newChannel;
     }
 
-    getMessages(ammount?: number, startIndex?: number) {
-        const endIndex = this._messages.length
-        const readAmmount = ammount || endIndex
-        const startFromIndex = startIndex || 0 + (endIndex - readAmmount)
-
-        return this._messages.slice(startFromIndex, endIndex)
+    public deleteChannel(channelId: ChannelIdentifier) {
+        this._removeChannel(channelId);
     }
+
+    public getChannelCount() {
+        return this._channels.size;
+    } 
+
+    public getChannelById(id: ChannelIdentifier): Channel | undefined {
+        return this._channels.get(id);
+    }
+
+    private _insertChannel(channel: Channel) {
+        this._channels.set(channel.id, channel);
+    }
+
+    private _removeChannel(channelId: ChannelIdentifier) {
+        this._channels.delete(channelId);
+    }
+
 }
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import socketio from 'socket.io'
 import http from 'http'
 import { slog } from './utils'
 import settings from './server_config.json'
-import { PUpdateNickname } from './packets'
 
 var DEBUG: boolean = true
 const PORT: any = process.env.PORT || 3000
@@ -21,11 +20,3 @@ httpServer.listen(PORT, () => {
 
 
 
-io.on('connection', (client) => {
-    DEBUG && slog.debug(`New connection: ${client.id}!`)
-
-    client.on('disconnect', (reason: String) => {
-        DEBUG && slog.debug(`Disconnection: ${client.id}! Reason: ${reason}`)
-        client.removeAllListeners()
-    })
-})
